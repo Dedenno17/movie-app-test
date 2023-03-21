e60151d37681dbcbf05d8457277678f3078152de
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-03-21 21:17:08 +0700

    menambahkan animasi hover pada content

--- src/components/Home/ContentList.tsx
+++ src/components/Home/ContentList.tsx
@@ -1,8 +1,12 @@
-import { titleFormat } from '@/helpers/titleFormat';
+import {
+  titleFormatSmallScreen,
+  titleFormatLargeScreen,
+} from '@/helpers/titleFormat';
 import { Content, ContentData } from '@/types';
 import Image from 'next/image';
 import React from 'react';
 import SkeletonLoading from './SkeletonLoading';
+import { BsPlayCircle } from 'react-icons/bs';
 
 interface Props {
   contentData: ContentData | undefined;
@@ -24,7 +28,7 @@ const ContentList: React.FC<Props> = ({ contentData, isLoading }) => {
               key={item.imdbID}
               className="w-full h-56 rounded-lg flex flex-col overflow-hidden lg:h-64 xl:h-96"
             >
-              <div className="w-full h-[75%] rounded-b-lg overflow-hidden">
+              <div className="w-full h-[75%] rounded-b-lg overflow-hidden cursor-pointer relative group">
                 <div className="relative w-full h-full">
                   <Image
                     src={
@@ -38,10 +42,12 @@ const ContentList: React.FC<Props> = ({ contentData, isLoading }) => {
                     priority
                   />
                 </div>
+                <div className="absolute top-0 left-0 bottom-0 right-0 opacity-0 bg-primaryBlack/60 transition duration-500 backdrop-blur-sm group-hover:opacity-100" />
+                <BsPlayCircle className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-primaryWhite z-10 text-6xl scale-50 opacity-0 transition duration-500 group-hover:scale-100 group-hover:opacity-100" />
               </div>
               <div className="w-full h-[25%] flex flex-col justify-center gap-1 px-2 py-1">
-                <span className="text-sm text-primaryWhite font-semibold">
-                  {titleFormat(item.Title)}
+                <span className="text-sm text-primaryWhite font-semibold cursor-pointer hover:text-primaryRed">
+                  {titleFormatSmallScreen(item.Title)}
                 </span>
                 <span className="text-xs text-primaryGrey">{`(${item.Year})`}</span>
               </div>
--- src/helpers/titleFormat.ts
+++ src/helpers/titleFormat.ts
@@ -1,9 +1,20 @@
-export const titleFormat = (str: string): string => {
+export const titleFormatSmallScreen = (str: string): string => {
   let formatedTitle = str;
   const splitTitle = str.split('');
 
   if (splitTitle.length > 10) {
     formatedTitle = splitTitle.slice(0, 10).join('') + ' ...';
+  }
+
+  return formatedTitle;
+};
+
+export const titleFormatLargeScreen = (str: string): string => {
+  let formatedTitle = str;
+  const splitTitle = str.split('');
+
+  if (splitTitle.length > 10) {
+    formatedTitle = splitTitle.slice(0, 19).join('') + ' ...';
   }
 
   return formatedTitle;
